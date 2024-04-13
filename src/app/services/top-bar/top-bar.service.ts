485151ed3542a0e6c55b1fb3f320197566ddeef1
Msuf123 <akshatmalik18t@gmail.com>
2024-04-13 14:38:05 +0530

    made the top Bar button toogle themes

---
+++ src/app/services/top-bar/top-bar.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TopBarService {
+ icon='./../../../assets/icon.png'
+ loginIcon='../../../assets/Loginuser.png'
+}
