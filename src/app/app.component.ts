7168c3dbda6587de946d067b835000f12277ba94
Msuf123 <akshatmalik18t@gmail.com>
2024-04-14 21:51:03 +0530

    Stated my work on explore Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, DoCheck, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './components/top-bar/top-bar/top-bar.component';
 import { ThemeService } from './services/theme/theme.service';
+import { ExploreComponent } from './components/explore/explore.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TopBarComponent],
+  imports: [RouterOutlet,TopBarComponent,ExploreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers:[ThemeService]
