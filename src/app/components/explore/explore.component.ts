25b5a785482645256af35dedfbb733e4391692bb
Msuf123 <akshatmalik18t@gmail.com>
2024-06-03 08:41:04 +0530

    Started my work on bottom bar

--- src/app/components/explore/explore.component.ts
+++ src/app/components/explore/explore.component.ts
@@ -4,11 +4,12 @@ import { RecipeCardComponent } from './recipe-card/recipe-card.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { Router, RouterOutlet } from '@angular/router';
 import { TopBarComponent } from '../top-bar/top-bar/top-bar.component';
+import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
 
 @Component({
   selector: 'app-explore',
   standalone: true,
-  imports: [SearchBarComponent,RecipeCardComponent,SideBarComponent,RouterOutlet,TopBarComponent,ExploreComponent],
+  imports: [SearchBarComponent,RecipeCardComponent,SideBarComponent,RouterOutlet,TopBarComponent,ExploreComponent,BottomBarComponent],
   templateUrl: './explore.component.html',
   styleUrl: './explore.component.css'
 })
