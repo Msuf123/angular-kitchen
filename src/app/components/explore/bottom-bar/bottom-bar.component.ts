abe947e41039d610d8f4d8750c19f39b6ef908c2
Msuf123 <akshatmalik18t@gmail.com>
2024-06-03 22:36:57 +0530

    Found the solution

--- src/app/components/explore/bottom-bar/bottom-bar.component.ts
+++ src/app/components/explore/bottom-bar/bottom-bar.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-bottom-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,NgOptimizedImage],
   templateUrl: './bottom-bar.component.html',
   styleUrl: './bottom-bar.component.css'
 })
 export class BottomBarComponent {
-
+ buttons:{path:string,img:string}[]=[{path:'Home',img:'../../../../assets/home.png'},{path:'Inbox',img:'inbox.png'},{path:'Write',img:'edit.png'},{path:'Profile',img:'user.png'}]
 }
