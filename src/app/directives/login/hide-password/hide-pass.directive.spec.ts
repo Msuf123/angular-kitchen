59bc4f4847ec12b2348387aeee48942ab956d5b8
Msuf123 <akshatmalik18t@gmail.com>
2024-06-05 18:35:40 +0530

    Made the direcitve for the hide password

---
+++ src/app/directives/login/hide-password/hide-pass.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HidePassDirective } from './hide-pass.directive';
+
+describe('HidePassDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HidePassDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/login/hide-password/hide-pass.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appHidePass]',
+  standalone: true
+})
+export class HidePassDirective {
+
+  constructor() { }
+
+}
