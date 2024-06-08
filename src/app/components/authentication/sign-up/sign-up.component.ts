21a1f713dbd17f20c4b300eb69d57ce45b405684
Msuf123 <akshatmalik18t@gmail.com>
2024-06-08 21:07:57 +0530

    Random commit

--- src/app/components/authentication/sign-up/sign-up.component.ts
+++ src/app/components/authentication/sign-up/sign-up.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
-
+ formBuilder=inject(FormBuilder)
+ signUpForm=this.formBuilder.group({
+  username:[''],
+  password:[''],
+  email:['']
+ })
+ formSubmitted(){
+  console.log(this.signUpForm.value)
+ }
 }
