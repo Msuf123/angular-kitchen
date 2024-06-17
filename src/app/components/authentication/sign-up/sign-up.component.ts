7c5888ac538a60126cb00a0dcc9cd3b004bb7ade
Msuf123 <akshatmalik18t@gmail.com>
2024-06-17 23:38:15 +0530

    Added a for loop

--- src/app/components/authentication/sign-up/sign-up.component.ts
+++ src/app/components/authentication/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { Component, DoCheck, Inject, inject } from '@angular/core';
+import { CSP_NONCE, Component, DoCheck, Inject, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { HttpServiceService } from '../../../services/global-http/http-service.service';
 import { url } from '../../../app.config';
@@ -12,7 +12,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
-export class SignUpComponent implements DoCheck{
+export class SignUpComponent{
  formBuilder=inject(FormBuilder)
  httpService=inject(HttpServiceService)
  fileDataUrl=''
@@ -23,14 +23,21 @@ export class SignUpComponent implements DoCheck{
   email:[''],
   files:['']
  })
- ngDoCheck(): void {
-   console.log(this.fileDataUrl)
- }
+ 
  fileUploaded(evnet:any){
   
   const files:File=evnet.target.files[0]
   const reder=new FileReader()
-
+  const arrayBuffer:any=files.arrayBuffer().then((a)=>{
+    const arrayBufferViewer=new Int8Array(a)
+    console.log(arrayBufferViewer.length,arrayBufferViewer.byteLength)
+    let sizeOfArrays=50000
+    let noOfChunks=arrayBufferViewer.byteLength/sizeOfArrays
+    for(let i=0;i<noOfChunks+1;i++){
+     
+    console.log(arrayBufferViewer.slice(i*sizeOfArrays,(i+1)*sizeOfArrays))
+    }
+  })
   reder.readAsDataURL(files)
   reder.onload=()=>{
     this.fileDataUrl=reder.result as string
