0d06b6df6a14992fe7c738ca8109016f918c5d70
Msuf123 <akshatmalik18t@gmail.com>
2024-06-19 10:09:22 +0530

    Used the FileReaderService

--- src/app/components/creator-sections/writing-recipe/individual-input-form/individual-input-form.component.ts
+++ src/app/components/creator-sections/writing-recipe/individual-input-form/individual-input-form.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import BaseQuestion from '../../../../custom-class/questions-class/creator-write-sec/base.question';
+import { ReadFilesService } from '../../../../services/readFile-single/read-files.service';
 
 @Component({
   selector: 'app-individual-input-form',
@@ -11,6 +12,7 @@ import BaseQuestion from '../../../../custom-class/questions-class/creator-write
   styleUrl: './individual-input-form.component.css'
 })
 export class IndividualInputFormComponent {
+  filereaderService=inject(ReadFilesService)
   classNames={focused:false}
 @Input() formGroup!:FormGroup
 @Input() question!:BaseQuestion
@@ -25,14 +27,9 @@ ngAfterViewInit(){
     this.errors=this.formGroup.get(this.question.key)?.hasError('required') as boolean 
   })
 }
-fileUploaded(event:any){
-let files:File[]=event.target.files
-const reader=new FileReader()
-
-reader.readAsDataURL(files[0])
-reader.onload=(event: ProgressEvent<FileReader>)=>{
-  this.url=reader.result as string
-}
+async fileUploaded(event:any){
+const response=await this.filereaderService.raedFile(event.target)
+this.url=response as string
 }
 
 deleteImage(){
