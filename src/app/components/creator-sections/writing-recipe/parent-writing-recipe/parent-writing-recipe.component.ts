50b535c66737c7c1461611a84e6e95883c6aea6f
Msuf123 <akshatmalik18t@gmail.com>
2024-09-11 04:14:34 +0530

    Remvoed the if stemtn

--- src/app/components/creator-sections/writing-recipe/parent-writing-recipe/parent-writing-recipe.component.ts
+++ src/app/components/creator-sections/writing-recipe/parent-writing-recipe/parent-writing-recipe.component.ts
@@ -1,75 +1,83 @@
-import { Component, ViewEncapsulation, inject } from '@angular/core';
-import { WritingTopBarComponent } from './writing-top-bar/writing-top-bar.component';
-import { WritingFirstSectionComponent } from './writing-first-section/writing-first-section.component';
-import { WritingSecondSectionComponent } from './writing-second-section/writing-second-section.component';
-import { WritingThirdSectionComponent } from './writing-third-section/writing-third-section.component';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { HttpServiceService } from '../../../../services/global-http/http-service.service';
-import { FormGeneratorServiceService } from '../../../../services/creator-sections/writing-recipe/form-generator-service.service';
-import { CommonModule } from '@angular/common';
-import { LoadingService } from '../../../../services/loading/loading.service';
-import { SignInMsgComponent } from '../../../authentication/sign-in-msg/sign-in-msg.component';
-import { SaveChangesService } from '../../../../services/creator-sections/should-save-changes/save-changes.service';
-import { DisplaySaveDraftComponent } from '../../display-save-draft/display-save-draft.component';
-import { DisplayMessageService } from '../../../../services/creator-sections/display-save-draft/display-message.service';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { Component, ViewEncapsulation, inject } from "@angular/core";
+import { WritingTopBarComponent } from "./writing-top-bar/writing-top-bar.component";
+import { WritingFirstSectionComponent } from "./writing-first-section/writing-first-section.component";
+import { WritingSecondSectionComponent } from "./writing-second-section/writing-second-section.component";
+import { WritingThirdSectionComponent } from "./writing-third-section/writing-third-section.component";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { HttpServiceService } from "../../../../services/global-http/http-service.service";
+import { FormGeneratorServiceService } from "../../../../services/creator-sections/writing-recipe/form-generator-service.service";
+import { CommonModule } from "@angular/common";
+import { LoadingService } from "../../../../services/loading/loading.service";
+import { SignInMsgComponent } from "../../../authentication/sign-in-msg/sign-in-msg.component";
+import { SaveChangesService } from "../../../../services/creator-sections/should-save-changes/save-changes.service";
+import { DisplaySaveDraftComponent } from "../../display-save-draft/display-save-draft.component";
+import { DisplayMessageService } from "../../../../services/creator-sections/display-save-draft/display-message.service";
+import { BehaviorSubject, Subject } from "rxjs";
 @Component({
-  selector: 'app-parent-writing-recipe',
+  selector: "app-parent-writing-recipe",
   standalone: true,
-  imports: [WritingTopBarComponent,DisplaySaveDraftComponent,CommonModule,WritingFirstSectionComponent,WritingSecondSectionComponent,WritingThirdSectionComponent,ReactiveFormsModule,SignInMsgComponent],
-  templateUrl: './parent-writing-recipe.component.html',
-  styleUrl: './parent-writing-recipe.component.css',
-  encapsulation:ViewEncapsulation.ShadowDom
+  imports: [
+    WritingTopBarComponent,
+    DisplaySaveDraftComponent,
+    CommonModule,
+    WritingFirstSectionComponent,
+    WritingSecondSectionComponent,
+    WritingThirdSectionComponent,
+    ReactiveFormsModule,
+    SignInMsgComponent,
+  ],
+  templateUrl: "./parent-writing-recipe.component.html",
+  styleUrl: "./parent-writing-recipe.component.css",
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class ParentWritingRecipeComponent {
-  form:FormGroup
-  listOfQuestioins
-  signedIn=false
-  loadingService=inject(LoadingService)
-  canDeactivateS=inject(SaveChangesService)
-  showDisplay=inject(DisplayMessageService)
-  showMsg=false
-  counter=0
-  shouldDeactivate=new Subject<boolean>()
-   constructor(private questions:HttpServiceService,private formService:FormGeneratorServiceService){
-    questions.get('/write/auth').subscribe((res)=>{
-      console.log(res)
-      if(res==='okay'){
-        this.signedIn=true
+  form: FormGroup;
+  listOfQuestioins;
+  signedIn = false;
+  loadingService = inject(LoadingService);
+  canDeactivateS = inject(SaveChangesService);
+  showDisplay = inject(DisplayMessageService);
+  showMsg = false;
+  counter = 0;
+  shouldDeactivate = new Subject<boolean>();
+  constructor(
+    private questions: HttpServiceService,
+    private formService: FormGeneratorServiceService,
+  ) {
+    questions.get("/write/auth").subscribe((res) => {
+      console.log(res);
+      //res==='okay'
+      if (true) {
+        this.signedIn = true;
       }
-    })
-    
-    this.listOfQuestioins=this.questions.question()
-     this.form=formService.getFormObject(this.listOfQuestioins)
-     this.form.valueChanges.subscribe((a)=>{
-      console.log(a)
-      if(this.counter===0){
-          this.canDeactivateS.saveCahnges.next(true)
-          console.log("Made changes")
-          this.counter++
+    });
+
+    this.listOfQuestioins = this.questions.question();
+    this.form = formService.getFormObject(this.listOfQuestioins);
+    this.form.valueChanges.subscribe((a) => {
+      console.log(a);
+      if (this.counter === 0) {
+        this.canDeactivateS.saveCahnges.next(true);
+        console.log("Made changes");
+        this.counter++;
       }
-      
-     })
-    this.showDisplay.shouldDisplay.subscribe((state)=>{
-      this.showMsg=state
-    })
-   }
-   submitForm(e:any){
-    let eventEmitter=e.submitter
-    if(eventEmitter.value==='Publish'){
-      console.log(this.form.value)
-    }
-    
-    
-   }
-   canDeactivate() {
-    if(this.counter===0){
-      return true
-    }
-    this.showMsg=true
-    console.log("Calling can dev")
-    return this.shouldDeactivate
-    
+    });
+    this.showDisplay.shouldDisplay.subscribe((state) => {
+      this.showMsg = state;
+    });
   }
-   
+  submitForm(e: any) {
+    let eventEmitter = e.submitter;
+    if (eventEmitter.value === "Publish") {
+      console.log(this.form.value);
+    }
+  }
+  canDeactivate() {
+    if (this.counter === 0) {
+      return true;
+    }
+    this.showMsg = true;
+    console.log("Calling can dev");
+    return this.shouldDeactivate;
+  }
 }
