043a4116ab371cd31a5b5f5eeccf30f960f5ccc5
Msuf123 <akshatmalik18t@gmail.com>
2024-09-10 21:12:09 +0530

    Changed the twitter key

--- src/app/components/authentication/x-oauth/x-oauth.component.ts
+++ src/app/components/authentication/x-oauth/x-oauth.component.ts
@@ -1,56 +1,59 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { HttpServiceService } from '../../../services/global-http/http-service.service';
-import { adjectives, colors, uniqueNamesGenerator } from 'unique-names-generator';
+import { Component, OnInit, inject } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { HttpServiceService } from "../../../services/global-http/http-service.service";
+import {
+  adjectives,
+  colors,
+  uniqueNamesGenerator,
+} from "unique-names-generator";
 
 @Component({
-  selector: 'app-x-oauth',
+  selector: "app-x-oauth",
   standalone: true,
   imports: [],
-  templateUrl: './x-oauth.component.html',
-  styleUrl: './x-oauth.component.css'
+  templateUrl: "./x-oauth.component.html",
+  styleUrl: "./x-oauth.component.css",
 })
-export class XOauthComponent implements OnInit{
-  router=inject(ActivatedRoute)
-  http=inject(HttpServiceService)
-  twitterLogin=new URL('https://twitter.com/i/oauth2/authorize')
-  twitterSearchPrams=new URLSearchParams({
-    response_type:"code",
-    client_id:"Rmd2OG42UE5qeXBuX3FyMUN6REY6MTpjaQ",
-    redirect_uri:"https://angular-kitchen.vercel.app/oauth/x",
-    state:"2",
-    code_challenge:'jkdaf',
-    code_challenge_method:'plain',scope:'users.read+tweet.read'
-  })
+export class XOauthComponent implements OnInit {
+  router = inject(ActivatedRoute);
+  http = inject(HttpServiceService);
+  twitterLogin = new URL("https://twitter.com/i/oauth2/authorize");
+  twitterSearchPrams = new URLSearchParams({
+    response_type: "code",
+    client_id: "ZENKdnpmSjVSNlJHbDZvRDVMMm86MTpjaQ",
+    redirect_uri: "https://angular-kitchen.vercel.app/oauth/x",
+    state: "2",
+    code_challenge: "jkdaf",
+    code_challenge_method: "plain",
+    scope: "users.read+tweet.read",
+  });
   ngOnInit(): void {
-    const accessToken=new URL(window.location.href)
-     const searchPrmas=new URLSearchParams(accessToken.searchParams)
-    
-    if(!searchPrmas.has('code')){
-      const code=uniqueNamesGenerator({dictionaries:[adjectives,colors]})
-      console.log('Mkaing request with code',code)
-        this.http.post('/x-oauth/codeChallange',{code}).subscribe((a)=>{
-          if(a==='okay'){
-            this.twitterSearchPrams.set('code_challenge',code)
-           console.log(this.twitterSearchPrams.get(code))
-            window.location.href=this.twitterLogin.toString()+'?'+this.twitterSearchPrams.toString()
-          }
-        })
+    const accessToken = new URL(window.location.href);
+    const searchPrmas = new URLSearchParams(accessToken.searchParams);
+
+    if (!searchPrmas.has("code")) {
+      const code = uniqueNamesGenerator({ dictionaries: [adjectives, colors] });
+      console.log("Mkaing request with code", code);
+      this.http.post("/x-oauth/codeChallange", { code }).subscribe((a) => {
+        if (a === "okay") {
+          this.twitterSearchPrams.set("code_challenge", code);
+          console.log(this.twitterSearchPrams.get(code));
+          window.location.href =
+            this.twitterLogin.toString() +
+            "?" +
+            this.twitterSearchPrams.toString();
+        }
+      });
+    } else {
+      if (searchPrmas.has("code")) {
+        const code = searchPrmas.get("code");
+        this.http.post("/x-oauth/code", { code }).subscribe((a) => {
+          console.log(a);
+        });
+      } else {
+        console.log("Try again");
       }
-     else{
-     if(searchPrmas.has('code')){
-      const code=searchPrmas.get('code')
-      this.http.post('/x-oauth/code',{code}).subscribe((a)=>{
-        console.log(a)
-      })
-     }
-     else{
-      console.log('Try again')
-     }
     }
   }
-  constructor(){
-    
-  }
-    
+  constructor() {}
 }
