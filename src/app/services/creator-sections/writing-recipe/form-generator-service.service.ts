6e14a33a23fe79997a27e230c364a3af7f9859c2
Msuf123 <akshatmalik18t@gmail.com>
2024-09-11 04:50:32 +0530

    Made a little bit styling according to the mobile

--- src/app/services/creator-sections/writing-recipe/form-generator-service.service.ts
+++ src/app/services/creator-sections/writing-recipe/form-generator-service.service.ts
@@ -1,28 +1,31 @@
-import { Injectable } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import BaseQuestion from '../../../custom-class/questions-class/creator-write-sec/base.question';
+import { Injectable } from "@angular/core";
+import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
+import BaseQuestion from "../../../custom-class/questions-class/creator-write-sec/base.question";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FormGeneratorServiceService {
-
-  constructor() { }
-  getFormObject(arrayOfQuestions:BaseQuestion[]){
-    let  object:any={
-      
-      ingridents:new FormArray([new FormControl('',{validators:[Validators.required]})]),
-      steps:new FormArray([new FormGroup({
-        heading:new FormControl(''),
-        about:new FormControl(''),
-        imageUrl:new FormControl('')
-      })])
-    }
-    arrayOfQuestions.forEach(element => {
-      object[element.key]= element.required?new FormControl('',{validators:[Validators.required]}): new FormControl(element.defaultValue)
-
+  constructor() {}
+  getFormObject(arrayOfQuestions: BaseQuestion[]) {
+    let object: any = {
+      description: new FormControl(""),
+      ingridents: new FormArray([
+        new FormControl("", { validators: [Validators.required] }),
+      ]),
+      steps: new FormArray([
+        new FormGroup({
+          heading: new FormControl(""),
+          about: new FormControl(""),
+          imageUrl: new FormControl(""),
+        }),
+      ]),
+    };
+    arrayOfQuestions.forEach((element) => {
+      object[element.key] = element.required
+        ? new FormControl("", { validators: [Validators.required] })
+        : new FormControl(element.defaultValue);
     });
-    return new FormGroup(object)
+    return new FormGroup(object);
   }
- 
 }
