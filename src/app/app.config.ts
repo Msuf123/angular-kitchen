99f9654ac1ba43311cc635e02c6dbe24139b0de3
Msuf123 <akshatmalik18t@gmail.com>
2024-10-21 04:06:27 +0530

    added a url

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,5 +6,5 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { provideCloudflareLoader, provideCloudinaryLoader } from '@angular/common';
 export const url=new InjectionToken<string>('backendUrl')
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),importProvidersFrom(HttpClientModule),provideCloudinaryLoader('https://res.cloudinary.com/dglvbq5bx'),{provide:url,useValue:'http://bl57hzvkqg3mu6xvzxyow5rthzva32yuq2qwkddhpeqqqhv74wlcvhyd.onion'}]
+  providers: [provideRouter(routes),importProvidersFrom(HttpClientModule),provideCloudinaryLoader('https://res.cloudinary.com/dglvbq5bx'),{provide:url,useValue:'http://ec2-3-107-208-87.ap-southeast-2.compute.amazonaws.com:3000'}]
 };
