3967a6d38b9fe377d43e2ab4d08c05fb921cf401
Msuf123 <akshatmalik18t@gmail.com>
2024-10-22 06:09:28 +0530

    Made erro handling meas while authenitcating

--- src/app/components/authentication/x-oauth/x-oauth.component.ts
+++ src/app/components/authentication/x-oauth/x-oauth.component.ts
@@ -42,21 +42,27 @@ export class XOauthComponent implements OnInit {
               this.http.post("/x-oauth/codeChallange", { code }).subscribe((a) => {
                 if (a === "okay") {
                   this.twitterSearchPrams.set("code_challenge", code);
-                  console.log(this.twitterSearchPrams.get(code));
+                  
                   window.location.href =
                     this.twitterLogin.toString() +
                     "?" +
                     this.twitterSearchPrams.toString();
+                }
+                else{
+                  this.nav.navigate(['login'])
+                  
                 }
               });
             } else {
               if (searchPrmas.has("code")) {
                 const code = searchPrmas.get("code");
                 this.http.post("/x-oauth/code", { code }).subscribe((a) => {
-                  console.log(a);
+                 if(a==="okay"){
+                  this.nav.navigate([''])
+                 }
                 });
               } else {
-                console.log("Try again");
+                this.nav.navigate(['login'])
               }
             }
   }
