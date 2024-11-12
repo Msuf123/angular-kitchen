53f2bf7feaae93b22b5924930faf5bb22ce2cb51
Msuf123 <akshatmalik18t@gmail.com>
2024-11-13 00:43:23 +0530

    Completed the icon naviagtion bar Second Target:Work area

--- src/app/components/planner/inner-components/common-component/plus-icon/plus-icon.component.ts
+++ src/app/components/planner/inner-components/common-component/plus-icon/plus-icon.component.ts
@@ -1,12 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-plus-icon',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './plus-icon.component.html',
   styleUrl: './plus-icon.component.css'
 })
 export class PlusIconComponent {
-
+  classes={mouseLeft:true,mouseOver:false}
+  classesForRight={mouseLeftRight:true,mouseOverRight:false}
+  leftPostion=100
+  opacityLeft=0
+  rightPostion=6
+  opacityRight=0
+  
+  entered(){
+    for(let i=0;i<10;i++){
+      setTimeout(()=>{
+        this.leftPostion-=10
+        this.rightPostion+=4.3
+        this.opacityLeft+=0.1
+      },i*10)
+      
+    }
+  }
+  left(){
+    for(let i=0;i<10;i++){
+      setTimeout(()=>{
+        this.leftPostion+=10
+        this.rightPostion-=4.3
+        this.opacityLeft-=0.1
+      },i*10)
+      
+    }
+   
+  }
+  
 }
